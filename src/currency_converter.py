6fc1906787d05b2ca252bbb969a2f2e2c8270008
Ryan Fehr <coderry4@gmail.com>
2025-08-16 18:16:00 -0500

    Adds getCurrencies function to display currencies using API

---
+++ src/currency_converter.py
@@ -0,0 +1,20 @@
+
+from requests import get
+from pprint import PrettyPrinter
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+BASE_URL = "https://api.freecurrencyapi.com/v1/latest"
+
+printer = PrettyPrinter()
+
+def getCurrencies():
+    url = f"{BASE_URL}?apikey={API_KEY}"
+    data = get(url).json()
+    
+    
+
+getCurrencies()
