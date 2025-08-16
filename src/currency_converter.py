138fe7e671c3ad70771139ec04ae037a690fed82
Ryan Fehr <coderry4@gmail.com>
2025-08-16 18:25:44 -0500

    Fix API to receive currency data with latest rate

--- src/currency_converter.py
+++ src/currency_converter.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-BASE_URL = "https://api.freecurrencyapi.com/v1/latest"
+BASE_URL = "https://api.freecurrencyapi.com/v1/currencies"
 
 printer = PrettyPrinter()
 
 def getCurrencies():
     url = f"{BASE_URL}?apikey={API_KEY}"
     data = get(url).json()
-    
-    
+    printer.pprint(data)
+
 
 getCurrencies()
