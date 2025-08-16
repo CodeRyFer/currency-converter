b614c96fa08449f96073d39cdb90db909cfaad72
Ryan Fehr <coderry4@gmail.com>
2025-08-16 18:44:36 -0500

    Adds print_currencies to display currency code, name, and symbol

--- src/currency_converter.py
+++ src/currency_converter.py
@@ -11,10 +11,23 @@ BASE_URL = "https://api.freecurrencyapi.com/v1/currencies"
 
 printer = PrettyPrinter()
 
-def getCurrencies():
+def get_currencies():
     url = f"{BASE_URL}?apikey={API_KEY}"
-    data = get(url).json()
-    printer.pprint(data)
+    data = get(url).json()['data']
+
+    data = list(data.items())
+
+    data.sort()
+
+    return data
+
+def print_currencies(currencies):
+    for _id, currency in currencies:
+        name = currency.get('name', '')
+        symbol = currency.get('symbol', '')
+        print(f"{_id} - {name} - {symbol}")
 
 
-getCurrencies()
+data = get_currencies()
+#printer.pprint(data)
+print_currencies(data)
