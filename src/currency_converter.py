fcc15411fc82a229d97a73a1981a7fbd7c1a1504
Ryan Fehr <coderry4@gmail.com>
2025-08-16 19:23:10 -0500

    Adds convert to display exchange rate between two currencies with an amount

--- src/currency_converter.py
+++ src/currency_converter.py
@@ -35,12 +35,29 @@ def exchange_rate(currency1, currency2):
     if len(data) == 0:
         print('Invalid currencies')
         return
+    
+    rate = list(data.values())[9]
+    print(f"{currency1} -> {currency2} = {rate}")
 
-    return list(data.values())[9]
+    return rate
+    
+def convert(currency1, currency2, amount):
+    rate = exchange_rate(currency1, currency2)
+    if rate is None:
+        return
+    
+    try: amount = float(amount)
+    except:
+        print("Invalid amount")
+        return
+    
+    converted_amount = rate * amount
+    print(f"{amount} {currency1} is equal to = {converted_amount} {currency2}")
+    return converted_amount
+
 
 #data = get_currencies()
 #printer.pprint(data)
 #print_currencies(data)
 
-rate = exchange_rate("USD", "CAD")
-print(rate)
+convert("USD", "CAD", 30)
